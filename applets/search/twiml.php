7d4def8a5cc0515d88ba8331df85eb3e81ad4051
Tim Lytle <tim@timlytle.net>
2010-06-26 23:26:26 -0400

    Refactored code. Added comments. Fixed a few bugs.

--- applets/search/twiml.php
+++ applets/search/twiml.php
@@ -1,126 +1,178 @@
 <?php
+//Needed because we're unserializing objects from the session, and this class
+//isn't loaded.
 include_once(APPPATH.'models/vbx_device.php');
+
+//this should help with namespace problems
 define(SESSION_KEY, AppletInstance::getInstanceId());
 session_start();
 
-//first time through
-if(!isset($_REQUEST['Digits']) AND !isset($_REQUEST['DialStatus'])){
-    //prompt for search
-    return searchPrompt(new Response());
-}
+//Note: in reverse chronological order for simpler condition tests, 'return' is
+//used to stop further processing
 
-//second time through
-if(!isset($_SESSION[SESSION_KEY]['users']) AND !isset($_REQUEST['DialStatus'])){
-    //find and list users
-    $users = getMatches($_REQUEST['Digits']);
-    if(0 == count($users)){
-        $response = new Response();
-        $response->append(new Say("Sorry, no matches found."));
-        return searchPrompt($response);
-    }
-
-    $_SESSION[SESSION_KEY]['users'] = serialize($users);
-    return promptMenu(new Response(), $users);
-
-//third time through
-} elseif(isset($_SESSION[SESSION_KEY]['users'])) {
-    //dial selected user
-    $index = $_REQUEST['Digits'];
-    $users = unserialize($_SESSION[SESSION_KEY]['users']);
-    if("0" == $index){
-        //0 resets the process
-        $response = new Response();
-        $response->addSay("Starting over.");
-        return searchPrompt($response);
-    } elseif(!isset($users[$index - 1])){
-        //send the menu again, the entry wasn't found
-        $response = new Response();
-        $response->addSay("Please select a user.");
-        return promptMenu($response, $users);
-    }
-
-    unset($_SESSION[SESSION_KEY]['users']);
-    
-    $user = $users[$index - 1];
-
-    $_SESSION[SESSION_KEY]['user'] = serialize($user);
-    $_SESSION[SESSION_KEY]['number'] = 0;
-
-    $response = new Response();
-
-    $response->addSay("Connecting you to {$user->first_name} {$user->last_name}");
-    connect($response, $user);
-    return;
-//try all devices
-} elseif(isset($_SESSION[SESSION_KEY]['user'])){
+//User in session means the call is being connected or it's been routed to
+//voicemail. Both actions requre the user object.
+if(isset($_SESSION[SESSION_KEY]['user'])){
+    //turn the user back into an object
     $user = unserialize($_SESSION[SESSION_KEY]['user']);
 
-    if(isset($_SESSION[SESSION_KEY]['voicemail'])){
+    //save the voicemail
+    if(isset($_REQUEST['RecordingUrl'])){
         OpenVBX::addVoiceMessage(
-            $user->id,
+            $user,
             $_REQUEST['CallGuid'],
             $_REQUEST['Caller'],
             $_REQUEST['Called'],
             $_REQUEST['RecordingUrl'],
             $_REQUEST['Duration']);
+
+        return;
+
+    //dialstatus means an attempt to connect
     } elseif(isset($_REQUEST['DialStatus'])) {
         if('answered' == $_REQUEST['DialStatus']){
             $response = new Response();
             $response->addHangup();
             $response->Respond();
+
             return;
         }
-        connect(new Response(), $user);
-        return;
+        return connect(new Response(), $user)->Respond();
+
     }
+
+    //something's wrong, let the caller know and start over.
+    return searchPrompt(errorResponse(new Response()))->Respond();
 }
 
-//if nothing responded, somethign is wrong
-$response = new Response();
-$response->addSay("Something went wrong.");
-$response->Respond();
+//Users in session means the caller should have selected a user.
+if(isset($_SESSION[SESSION_KEY]['users'])) {
+    //turn users back into an array of user objects
+    $users = unserialize($_SESSION[SESSION_KEY]['users']);
 
+    $index = $_REQUEST['Digits'];
+    //dialing '0' will restart the process
+    if("0" == $index){
+        return searchPrompt(addMessage(new Response(), 'restartMessage', 'Starting over.'))->Respond();
+    //check if the input is valid (note an offset of 1).
+    } elseif(!isset($users[$index - 1])){
+        //send the menu again, the entry wasn't found
+        return promptMenu(addMessage(new Response(), 'invalidMessage', 'Not a valid selection.'), $users)->Respond();
+    }
+
+    //the caller selected a user, unset the 'users' session var
+    unset($_SESSION[SESSION_KEY]['users']);
+
+    //get the selected user
+    $user = $users[$index - 1];
+
+    //store the user in session and set the device index to 0
+    $_SESSION[SESSION_KEY]['user'] = serialize($user);
+    $_SESSION[SESSION_KEY]['number'] = 0;
+
+    $response = new Response();
+    $response->addSay("Connecting you to {$user->first_name} {$user->last_name}");
+    return connect($response, $user)->Respond();
+}
+
+//Digits in the request, with no users or user is session means the caller is
+//searching for a user.
+if(isset($_REQUEST['Digits'])){
+    //find any matching users
+    $users = getMatches($_REQUEST['Digits']);
+    if(0 == count($users)){
+        return searchPrompt(addMessage(new Response(), 'nomatchMessage', 'Sorry, no matches found.'))->Respond();
+    }
+
+    //add users to the session so the next request can select one
+    $_SESSION[SESSION_KEY]['users'] = serialize($users);
+    return promptMenu(addMessage(new Response(), 'menuPrompt', 'Please select a user, or press 0 to search again.'), $users)->Respond();
+}
+
+//prompt for search
+return searchPrompt(new Response())->Respond();
+
+//Essentially the logic of the dial applet in a different form.
 function connect($response, $user)
 {
+    //get a name to announce the call
     $name = $user->first_name . " " . $user->last_name;
-    //get the current device id and increment for the next time
+    
+    //get the current device id and increment for the next attempt
     $device = $_SESSION[SESSION_KEY]['number']++;
-    //try to connect the current device
 
+    //try to connect the current device (if it exists)
     if(isset($user->devices[$device])){
         if(!$user->devices[$device]->is_active){
             //just try the next device
             return connect($response, $user);
         }
+        //add a dial to the response
         $dial = $response->addDial(array('action' => current_url()));
+        //and a number to the dail, so the call can be announced.
         $dial->addNumber($user->devices[$device]->value, array('url' => site_url('twiml/whisper?name='.urlencode($name))));
 
     //when there are no more devices send to voicemail
     } else {
-        $_SESSION[SESSION_KEY]['voicemail'] = true;
 	$response->append(AudioSpeechPickerWidget::getVerbForValue($user->voicemail, new Say("Please leave a message.")));
 	$response->addRecord(array(
-            'transcribe' => true,
+            'transcribe' => 'true',
             'transcribeCallback' => site_url('twiml/transcribe') ));
     }
-    $response->Respond();
+
+    //return the response so it can be modified
+    return $response;
 }
 
+//List the search results, and offset the index by 1 because dialing 0 generally
+//means something.
 function promptMenu($response, $users)
 {
     $gather = $response->addGather();
     foreach($users as $index => $user){
         $pos = $index + 1;
         $gather->addSay("Dial $pos for {$user->first_name} {$user->last_name}");
-        //print_r($user);
     }
-
-    $response->Respond();
+    
+    //return the response so it can be modified
+    return $response;
 }
 
+//Inital prompt.
+function searchPrompt($response)
+{
+    //since this is called on restart, make sure we clear everything
+    unset($_SESSION[SESSION_KEY]);
+
+    //add a gather to the rseponse then add the prompt to the gather
+    addMessage($response->addGather(), 'searchPrompt', 'Please enter the first few letters of the name, followed by the pound sign.');
+    
+    //return the response so it can be modified
+    return $response;
+}
+
+function errorResponse($response)
+{
+    return addMessage($response, 'errorMessage', 'Sorry, an error occured.');
+}
+
+function addMessage($response, $name, $fallback)
+{
+    $message = AppletInstance::getAudioSpeechPickerValue($name);
+    $response->append(
+        AudioSpeechPickerWidget::getVerbForValue($message, new Say($fallback)));
+
+    return $response;
+}
+
+//Find users that possible match a series of digits.
+//TODO: It's likely better to work it the other way, generating a list of
+//numbers from the users and looking for a partial match.
 function getMatches($digits)
 {
     //setup dial pad letters
+    $dialpad[0] = array();
+    $dialpad[1] = array();
     $dialpad[2] = array('a','b','c');
     $dialpad[3] = array('d','e','f');
     $dialpad[4] = array('g','h','i');
@@ -130,48 +182,37 @@ function getMatches($digits)
     $dialpad[8] = array('t','u','v');
     $dialpad[9] = array('w','x','y', 'z');
 
-    //find all combinations
-    //TODO: likely better to work it the other way, generating a
-    //list from the users and looking for a partial match.
+    //start with an empty string
     $words = array('');
     //take each digit
     for($i = 0; $i < strlen($digits); $i++){
         $digit = $digits[$i];
         $oldWords = $words;
         $words = array();
-        //and add all if its digits to every previous possibility
+        //and add all possible letters to every previous combination
         foreach($oldWords as $word){
             foreach($dialpad[$digit] as $letter){
                 $words[] = $word . $letter;
             }
         }
     }
-    //TODO: well now, this looks a little messy should find a better way to
-    //query for all matching names. Or again, work it the other way, building
-    //a number for each user
+
+    if(count($words) == 0){
+        return array();
+    }
+
+    //TODO: This looks a little messy - should find a better way to query for
+    //all matching names. Or again, work it the other way, building a number
+    //for each user.
 
     //create crazy query
     foreach($words as $word){
         $search[] = "first_name LIKE '%$word%' OR last_name LIKE '%$word%'";
     }
+
     $query = implode(" OR ", $search);
 
     //get all users that are possible matches
     $users = OpenVBX::getUsers(array($query . 'AND true = ' => 1));
     return $users;
-}
-
-function searchPrompt($response)
-{
-    //make sure we clear everything
-    unset($_SESSION[SESSION_KEY]);
-    //grab the search prompt
-    $prompt = AppletInstance::getAudioSpeechPickerValue('searchPrompt');
-    //play the search prompt
-    $response->addGather()
-        ->append(AudioSpeechPickerWidget::getVerbForValue($prompt,
-        new Say('Please enter the first few letters of the name, followed by the pound sign.')));
-    //get some digits
-    $response->Respond();
-    return;
 }
--- applets/search/ui.php
+++ applets/search/ui.php
@@ -1,7 +1,33 @@
 <div class="vbx-applet">
 	<h2>Directory Search</h2>
-	<p>Please select a prompt for callers. They will have to use 
-	the dial pad to search the user directory, ending with a 
-	pound (#) sign. <p>
+        <p>Callers can search for users by dialing a few letters form the user's
+        first or last name. The can press pound when done, or simply wair for 5
+        seconds.
+
+        <h3>Search Prompt</h3>
+        <p>When the caller reaches this menu they will hear this prompt:</p>
 	<?php echo AppletUI::audioSpeechPicker('searchPrompt'); ?>
+
+        <h3>Menu Prompt</h3>
+        <p>The caller can select a user from the menu, or dial 0 to try again.
+        Before the user menu is played, play this prompt:</p>
+        <?php echo AppletUI::audioSpeechPicker('menuPrompt'); ?>
+
+        <h3>No Matches</h3>
+        <p>Select a message to play when no users are found, before the process
+        is restarted.</p>
+        <?php echo AppletUI::audioSpeechPicker('nomatchMessage'); ?>
+
+        <h3>Restart Search</h3>
+        <p>The caller dialed 0 to restart the search, customize this message.</p>
+        <?php echo AppletUI::audioSpeechPicker('restartMessage'); ?>
+
+        <h3>Invalid Selection</h3>
+	<p>Customize a specific message about the invalid selection.</p>
+        <?php echo AppletUI::audioSpeechPicker('invalidMessage'); ?>
+        
+        <h3>Error Message</h3>
+        <p>Pick a message to notify the caller an error occurred before the
+        process is restarted.</p>
+        <?php echo AppletUI::audioSpeechPicker('errorMessage'); ?>
 </div>
